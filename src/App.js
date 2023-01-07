a4adfcd6f9e91c9b3770f3d2802653126118a3a7
Deveshb15 <deveshb583@gmail.com>
2023-01-07 15:11:11 +0530

    chore: tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
     </div>
   );
 }
