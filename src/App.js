d0657e955ec8f2167522d9cbf2989e0ab5aa360b
Deveshb15 <deveshb583@gmail.com>
2023-01-08 11:57:06 +0530

    feat: upload to IPFS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
 
+import { NFTStorage } from "nft.storage";
+
 function App() {
 	const [prompt, setPrompt] = useState("");
-  const [imageBlob, setImageBlob] = useState(null)
+	const [imageBlob, setImageBlob] = useState(null);
+	const [file, setFile] = useState(null);
 
 	console.log(prompt);
+
+  const cleanupIPFS = (url) => {
+    if(url.includes("ipfs://")) {
+      return url.replace("ipfs://", "https://ipfs.io/ipfs/")
+    }
+  }
 
 	const generateArt = async () => {
 		try {
@@ -20,34 +29,73 @@ function App() {
 				},
 				{ responseType: "blob" }
 			);
+			// convert blob to a image file type
+			const file = new File([response.data], "image.png", {
+				type: "image/png",
+			});
+			console.log(file);
+			setFile(file);
 			console.log(response);
-			const url = URL.createObjectURL(response.data)
+			const url = URL.createObjectURL(response.data);
 			// console.log(url)
-			console.log(url)
-      setImageBlob(url)
+			console.log(url);
+			setImageBlob(url);
 		} catch (err) {
 			console.log(err);
 		}
 	};
 
+  const uploadArtToIpfs = async () => {
+    try {
+
+      const nftstorage = new NFTStorage({
+				token: process.env.REACT_APP_NFT_STORAGE,
+			})
+
+      const store = await nftstorage.store({
+        name: file.name,
+        description: "AI generated NFT",
+        image: file
+      })
+
+      console.log(store)
+
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen gap-4">
 			<h1 className="text-4xl font-extrabold">AI Art Gasless mints</h1>
-      <div className="flex flex-col items-center justify-center">
-        {/* Create an input box and button saying next beside it */}
-        <div className="flex items-center justify-center gap-4">
-          <input
-            className="border-2 border-black rounded-md p-2"
-            onChange={(e) => setPrompt(e.target.value)}
-            type="text"
-            placeholder="Enter a prompt"
-          />
-          <button onClick={generateArt} className="bg-black text-white rounded-md p-2">Next</button>
-        </div>
-        {
-          imageBlob && <img src={imageBlob} alt="AI generated art" />
-        }
-      </div>
+			<div className="flex flex-col items-center justify-center">
+				{/* Create an input box and button saying next beside it */}
+				<div className="flex items-center justify-center gap-4">
+					<input
+						className="border-2 border-black rounded-md p-2"
+						onChange={(e) => setPrompt(e.target.value)}
+						type="text"
+						placeholder="Enter a prompt"
+					/>
+					<button
+						onClick={generateArt}
+						className="bg-black text-white rounded-md p-2"
+					>
+						Next
+					</button>
+				</div>
+				{imageBlob && (
+					<div className="flex flex-col gap-4 items-center justify-center">
+						<img src={imageBlob} alt="AI generated art" />
+						<button
+							onClick={uploadArtToIpfs}
+							className="bg-black text-white rounded-md p-2"
+						>
+							Mint NFT
+						</button>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
